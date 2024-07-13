4bdbe714222325b6c0c4ea1345e8f5f2b4f7f65e
kensuke.konishi <kensuke.konishi@systemi.co.jp>
2024-07-14 01:44:50 +0900

    chore: コード整形

--- docker-entrypoint-initdb.d/ddl.sql
+++ docker-entrypoint-initdb.d/ddl.sql
@@ -1,22 +1,31 @@
-CREATE TABLE `examinee` (
-                            `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
-                            PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='被験者';
+CREATE TABLE `examinee`
+(
+    `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='被験者';
 
-CREATE TABLE `wine` (
-                        `id` int unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',
-                        `toxic` tinyint(1) NOT NULL COMMENT '毒性の有無 TRUE:毒性あり',
-                        PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ワイン';
+CREATE TABLE `wine`
+(
+    `id`    int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'ID',
+    `toxic` tinyint(1)   NOT NULL COMMENT '毒性の有無 TRUE:毒性あり',
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='ワイン';
 
-CREATE TABLE `wine_ingestion_history` (
-                                          `id` int unsigned NOT NULL AUTO_INCREMENT,
-                                          `examinee_id` int unsigned NOT NULL,
-                                          `wine_id` int unsigned NOT NULL,
-                                          `ingestion_time` datetime NOT NULL,
-                                          PRIMARY KEY (`id`),
-                                          KEY `wine_history_wine` (`wine_id`),
-                                          KEY `wine_history_examinee` (`examinee_id`),
-                                          CONSTRAINT `wine_history_examinee` FOREIGN KEY (`examinee_id`) REFERENCES `examinee` (`id`),
-                                          CONSTRAINT `wine_history_wine` FOREIGN KEY (`wine_id`) REFERENCES `wine` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='ワイン摂取履歴';
+CREATE TABLE `wine_ingestion_history`
+(
+    `id`             int UNSIGNED NOT NULL AUTO_INCREMENT,
+    `examinee_id`    int UNSIGNED NOT NULL,
+    `wine_id`        int UNSIGNED NOT NULL,
+    `ingestion_time` datetime     NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `wine_history_wine` (`wine_id`),
+    KEY `wine_history_examinee` (`examinee_id`),
+    CONSTRAINT `wine_history_examinee` FOREIGN KEY (`examinee_id`) REFERENCES `examinee` (`id`),
+    CONSTRAINT `wine_history_wine` FOREIGN KEY (`wine_id`) REFERENCES `wine` (`id`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  COLLATE = utf8mb4_unicode_ci COMMENT ='ワイン摂取履歴';
--- src/main/java/jp/co/systemi/study/math_quiz/MathQuizApplication.java
+++ src/main/java/jp/co/systemi/study/math_quiz/MathQuizApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MathQuizApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MathQuizApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(MathQuizApplication.class, args);
+  }
 
 }
