cdd87cb4e61c98e9fea0fbc88afdd1ef5312a5f4
kensuke.konishi <kensuke.konishi@systemi.co.jp>
2024-07-14 11:33:24 +0900

    feat: モデルを追加

---
+++ src/main/java/jp/co/systemi/study/math_quiz/domain/Ingestion.java
@@ -0,0 +1,14 @@
+package jp.co.systemi.study.math_quiz.domain;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+
+@Data
+public class Ingestion implements Serializable {
+  private final int id;
+  private final int examineeId;
+  private final int wineId;
+  private final OffsetDateTime ingestionTime;
+}
---
+++ src/main/java/jp/co/systemi/study/math_quiz/domain/Wine.java
@@ -0,0 +1,9 @@
+package jp.co.systemi.study.math_quiz.domain;
+
+import lombok.Data;
+
+@Data
+public class Wine {
+  private final int id;
+  private final boolean toxic;
+}
